896ac614233474a2d5f07a32690b206c5238ecd5
Samarpit <nsamarpit25@gmail.com>
2025-01-21 08:35:01 +0530

    cart bug fix: cart gave error "not found" if cart was not created in the database

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -1,157 +1,163 @@
 import { createContext, FC, ReactNode, useEffect, useState } from "react";
 import {
-  CartItem,
-  CartItemAPI,
-  getCartState,
-  updateCartId,
-  updateCartItems,
-  updateCartState,
+ CartItem,
+ CartItemAPI,
+ getCartState,
+ updateCartId,
+ updateCartItems,
+ updateCartState,
 } from "../store/cart";
 import { useDispatch, useSelector } from "react-redux";
 import useAuth from "../hooks/useAuth";
 import client from "../api/client";
 import toast from "react-hot-toast";
 import { ParseError } from "../utils/helper";
+import { AxiosError } from "axios";
 
 interface CartApiResponse {
-  cart: {
-    id: string;
-    items: CartItemAPI[];
-  };
+ cart: {
+  id: string;
+  items: CartItemAPI[];
+ };
 }
 
 interface Props {
-  children: ReactNode;
+ children: ReactNode;
 }
 
 export interface ICartContext {
-  id?: string;
-  items: CartItem[];
-  pending: boolean;
-  fetching: boolean;
-  totalCount: number;
-  totalPrice: number;
-  subTotal: number;
-  updateCart(item: CartItem): void;
-  clearCart(): void;
+ id?: string;
+ items: CartItem[];
+ pending: boolean;
+ fetching: boolean;
+ totalCount: number;
+ totalPrice: number;
+ subTotal: number;
+ updateCart(item: CartItem): void;
+ clearCart(): void;
 }
 
 export const CartContext = createContext<ICartContext>({
-  items: [],
-  pending: false,
-  fetching: true,
-  totalCount: 0,
-  totalPrice: 0,
-  subTotal: 0,
-  updateCart() {},
-  clearCart() {},
+ items: [],
+ pending: false,
+ fetching: true,
+ totalCount: 0,
+ totalPrice: 0,
+ subTotal: 0,
+ updateCart() {},
+ clearCart() {},
 });
 
 const CART_KEY = "cartItems";
 const updateCartInLS = (cartItems: CartItem[]) => {
-  localStorage.setItem(CART_KEY, JSON.stringify(cartItems));
+ localStorage.setItem(CART_KEY, JSON.stringify(cartItems));
 };
 
 let startLSUpdate = false;
 
 const CartProvider: FC<Props> = ({ children }) => {
-  const cart = useSelector(getCartState);
-  const dispatch = useDispatch();
-  const { profile } = useAuth();
-  const [pending, setPending] = useState(false);
-  const [fetching, setFetching] = useState(true);
+ const cart = useSelector(getCartState);
+ const dispatch = useDispatch();
+ const { profile } = useAuth();
+ const [pending, setPending] = useState(false);
+ const [fetching, setFetching] = useState(true);
 
-  const clearCart = () => {
-    // update the UI
-    dispatch(updateCartState({ items: [], id: "" }));
+ const clearCart = () => {
+  // update the UI
+  dispatch(updateCartState({ items: [], id: "" }));
 
-    if (profile) {
-      // update the server/database
-      // if user is authenticated sending api request
-      setPending(true);
-      client
-        .post("/cart/clear")
-        .then(() => {
-          toast.success("Cart cleared successfully.");
-        })
-        .catch(ParseError)
-        .finally(() => {
-          setPending(false);
-        });
+  if (profile) {
+   // update the server/database
+   // if user is authenticated sending api request
+   setPending(true);
+   client
+    .post("/cart/clear")
+    .then(() => {
+     toast.success("Cart cleared successfully.");
+    })
+    .catch(ParseError)
+    .finally(() => {
+     setPending(false);
+    });
+  }
+ };
+
+ const updateCart = (item: CartItem) => {
+  startLSUpdate = true;
+  // update the UI
+  dispatch(updateCartItems(item));
+
+  if (profile) {
+   // update the server/database
+   // if user is authenticated sending api request
+   setPending(true);
+   client
+    .post("/cart", {
+     items: [{ product: item.product.id, quantity: item.quantity }],
+    })
+    .then(({ data }) => {
+     toast.success("Product added to cart.");
+     dispatch(updateCartId(data.cart));
+    })
+    .catch(ParseError)
+    .finally(() => {
+     setPending(false);
+    });
+  }
+ };
+
+ useEffect(() => {
+  if (startLSUpdate && !profile) {
+   updateCartInLS(cart.items);
+  }
+ }, [cart.items, profile]);
+
+ useEffect(() => {
+  const fetchCartInfo = async () => {
+   if (!profile) {
+    const result = localStorage.getItem(CART_KEY);
+    if (result) {
+     dispatch(updateCartState({ items: JSON.parse(result) }));
     }
+
+    return setFetching(false);
+   }
+
+   try {
+    const { data } = await client.get<CartApiResponse>("/cart");
+    dispatch(updateCartState({ id: data.cart.id, items: data.cart.items }));
+   } catch (error) {
+    if (error instanceof AxiosError) {
+     if (error.response?.status === 404) {
+      return;
+     }
+     ParseError(error);
+    }
+   } finally {
+    setFetching(false);
+   }
   };
 
-  const updateCart = (item: CartItem) => {
-    startLSUpdate = true;
-    // update the UI
-    dispatch(updateCartItems(item));
+  fetchCartInfo();
+ }, [dispatch, profile]);
 
-    if (profile) {
-      // update the server/database
-      // if user is authenticated sending api request
-      setPending(true);
-      client
-        .post("/cart", {
-          items: [{ product: item.product.id, quantity: item.quantity }],
-        })
-        .then(({ data }) => {
-          toast.success("Product added to cart.");
-          dispatch(updateCartId(data.cart));
-        })
-        .catch(ParseError)
-        .finally(() => {
-          setPending(false);
-        });
-    }
-  };
-
-  useEffect(() => {
-    if (startLSUpdate && !profile) {
-      updateCartInLS(cart.items);
-    }
-  }, [cart.items, profile]);
-
-  useEffect(() => {
-    const fetchCartInfo = async () => {
-      if (!profile) {
-        const result = localStorage.getItem(CART_KEY);
-        if (result) {
-          dispatch(updateCartState({ items: JSON.parse(result) }));
-        }
-
-        return setFetching(false);
-      }
-
-      try {
-        const { data } = await client.get<CartApiResponse>("/cart");
-        dispatch(updateCartState({ id: data.cart.id, items: data.cart.items }));
-      } catch (error) {
-        ParseError(error);
-      } finally {
-        setFetching(false);
-      }
-    };
-
-    fetchCartInfo();
-  }, [dispatch, profile]);
-
-  return (
-    <CartContext.Provider
-      value={{
-        id: cart.id,
-        items: cart.items,
-        totalCount: cart.totalCount,
-        subTotal: cart.subTotal,
-        totalPrice: cart.totalPrice,
-        pending,
-        fetching,
-        updateCart,
-        clearCart,
-      }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+ return (
+  <CartContext.Provider
+   value={{
+    id: cart.id,
+    items: cart.items,
+    totalCount: cart.totalCount,
+    subTotal: cart.subTotal,
+    totalPrice: cart.totalPrice,
+    pending,
+    fetching,
+    updateCart,
+    clearCart,
+   }}
+  >
+   {children}
+  </CartContext.Provider>
+ );
 };
 
 export default CartProvider;
