879e05765bd69382d8e3bbdb01b9ab04555d1d93
Samarpit <nsamarpit25@gmail.com>
2025-01-26 04:48:47 +0530

    feat: add js-cookie for handling authentication tokens; update base URL for development environment

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let baseURL = "https://ebook-server-9sh3.onrender.com";
 if (import.meta.env.MODE === "development") {
-  baseURL = "http://localhost:8989";
+  baseURL = "http://localhost:8000";
 }
 
 const client = axios.create({
--- src/views/Verify.tsx
+++ src/views/Verify.tsx
@@ -3,10 +3,13 @@ import { useDispatch } from "react-redux";
 import { Navigate, useSearchParams } from "react-router-dom";
 import { updateProfile } from "../store/auth";
 import LoadingSpinner from "../components/common/LoadingSpinner";
+import Cookies from "js-cookie";
 
 const Verify: FC = () => {
   const [searchParams] = useSearchParams();
   const profileInfoString = searchParams.get("profile");
+  const authToken = searchParams.get("authToken");
+  // console.log(authToken);
   const dispatch = useDispatch();
 
   if (profileInfoString) {
@@ -17,6 +20,13 @@ const Verify: FC = () => {
       }
 
       dispatch(updateProfile(profile));
+
+      Cookies.set("authToken", authToken || "", {
+        expires: 7, // Expiry in days
+        path: "", // Make it available for all paths
+        secure: true, // Ensure it's sent only over HTTPS
+        sameSite: "None", // Allow cross-site requests
+      });
 
       return <Navigate to="/" />;
     } catch (err) {
