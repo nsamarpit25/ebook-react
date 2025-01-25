8808daaf61be1529fbfae6fae7058c7a5f4e172e
Samarpit <nsamarpit25@gmail.com>
2025-01-26 02:22:04 +0530

    update API client base URL to production server

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  // baseURL: "https://ebook-server-9sh3.onrender.com",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://ebook-server-9sh3.onrender.com",
+  // baseURL: "http://localhost:8000",
   // baseURL: "http://192.168.1.9:8000/",
   // withCredentials: true,
 });
