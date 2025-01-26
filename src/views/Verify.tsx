14e6ef820e999abdd1ca51a071bb7d1fc1d44778
Samarpit <nsamarpit25@gmail.com>
2025-01-26 07:39:09 +0530

    fix: move authentication token cookie setting to ensure it is set only once

--- src/views/Verify.tsx
+++ src/views/Verify.tsx
@@ -9,24 +9,25 @@ const Verify: FC = () => {
   const [searchParams] = useSearchParams();
   const profileInfoString = searchParams.get("profile");
   const authToken = searchParams.get("authToken");
+
+  Cookies.set("authToken", authToken || "", {
+    expires: 7, // Expiry in days
+    path: "/", // Make it available for all paths
+    secure: true, // Ensure it's sent only over HTTPS
+    sameSite: "None", // Allow cross-site requests
+  });
   // console.log(authToken);
   const dispatch = useDispatch();
 
   if (profileInfoString) {
     try {
       const profile = JSON.parse(profileInfoString);
+      console.log(profile);
       if (!profile.signedUp) {
         return <Navigate to={"/new-user"} />;
       }
 
       dispatch(updateProfile(profile));
-
-      Cookies.set("authToken", authToken || "", {
-        expires: 7, // Expiry in days
-        path: "/", // Make it available for all paths
-        secure: true, // Ensure it's sent only over HTTPS
-        sameSite: "None", // Allow cross-site requests
-      });
 
       return <Navigate to="/" />;
     } catch (_err) {
